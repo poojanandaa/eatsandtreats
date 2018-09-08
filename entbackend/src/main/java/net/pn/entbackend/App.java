0e34cc4a11b6acf3af14b46c62f5ee6c3a82975d
Pooja Nanda <nanda.pooja52@gmail.com>
2018-09-08 14:47:14 +0530

    backend setup

---
+++ entbackend/src/main/java/net/pn/entbackend/App.java
@@ -0,0 +1,13 @@
+package net.pn.entbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
